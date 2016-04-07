f8e82dfd23b8e602df75ec3dbb31b75328dddd9e
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2016-04-07 06:54:39 -0500

    Modify square height

--- js/paint.js
+++ js/paint.js
@@ -5,7 +5,7 @@ var margin = {top: 20, right: 0, bottom: 20, left: 0},
 var formatNumber = d3.format(1);
 
 var y = d3.scale.linear()
-    .domain([0, 8])
+    .domain([0, 9])
     .range([height, 0]);
 
 var x = d3.scale.linear()
@@ -20,7 +20,8 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .tickSize(width)
-    .orient("right");
+    .orient("right")
+    .tickFormat("");
 
 var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
@@ -48,22 +49,22 @@ var bar = svg.selectAll(".bar")
     .data(data)
     .enter().append("g")
     .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + 0 + "," + y(d.y) + ")"; });
+    .attr("transform", function(d) { return "translate(" + 0 + "," + (y(d.y) - 51) + ")"; });
 
 function pintar_proceso(espera, comienzo, llegada, finalizacion, rafaga, length){
 	console.log("entra");
-	d3.select("svg").attr("height", Number(d3.select("svg").attr('height')) + 57);
-	d3.select("svg").attr("width", Number(d3.select("svg").attr('width')) + (57*10));
+	d3.select("svg").attr("height", Number(d3.select("svg").attr('height')) + 51);
+	d3.select("svg").attr("width", Number(d3.select("svg").attr('width')) + (31*10));
 	bar.append("rect")
 	    .attr("x", llegada*31) //x=5-->155
 	    .attr("class", "gris")
-	    .attr("y", -1*(height-(57*length)))
+	    .attr("y", -1*(height-(51*length)))
 	    .attr("width", espera*31)
-	    .attr("height", 57);
+	    .attr("height", 51);
 	bar.append("rect")
 	    .attr("x", comienzo*31)//x=5-->155
 	    .attr("class", "azul")
-	    .attr("y", -1*(height-(57*length)))
+	    .attr("y", -1*(height-(51*length)))
 	    .attr("width", rafaga*31)
-	    .attr("height", 57);
+	    .attr("height", 51);
 }
